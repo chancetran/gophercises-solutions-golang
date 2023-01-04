1ec7aa20e71ee6bf9f401b461b1df3507fa383a4
Chancellor Tran <chancetran98@gmail.com>
2023-01-03 20:32:55 -0800

    feat(quiz): implement part two

--- 1-quiz/main.go
+++ 1-quiz/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 /*
@@ -15,9 +17,12 @@ Sources
 - https://www.educative.io/answers/how-to-use-the-printf-function-in-golang
 - https://www.calhoun.io/6-tips-for-using-strings-in-go/#:~:text=Multiline%20strings,This%20is%20a%20multiline%20string
 - https://gobyexample.com/command-line-flags
+- https://go.dev/tour/concurrency/1
+- https://gobyexample.com/timeouts
 */
 
 var DEFAULT_FILEPATH = "problems.csv"
+var DEFAULT_TIME_LIMIT = 30
 
 // getQuizData reads in a file from the specified filepath and returns the
 // data.
@@ -39,6 +44,51 @@ func getQuizData(filepath string) [][]string {
 	return data
 }
 
+// executeQuiz loops over the specified data, displaying the first
+// row-attribute as the 'question', awaits user input, and then compares said
+// input with the second row-attribute as the 'answer'.
+//
+// If the user input matches the 'answer' the running total, or score, of
+// correct answers is incremented. Otherwise, the loop continues.
+//
+// The loop is terminated if the function execution time exceeds the
+// 'timeLimit'.
+func executeQuiz(data [][]string, timeLimit int) int {
+
+	var userResponse string
+
+	resultChannel := make(chan int, 1)
+	timeoutChannel := time.After(time.Duration(timeLimit) * time.Second)
+
+	score := 0
+	for i, row := range data {
+		fmt.Printf("%d. %s?\n", i+1, row[0])
+
+		go func() {
+			// TODO: CHANGE TO BUFIO SO IT CAN ACCEPT STRINGS WITH SPACES.
+			fmt.Scan(&userResponse)
+
+			result := 0
+			if userResponse == row[1] {
+				result = 1
+			}
+
+			resultChannel <- result
+		}()
+
+		select {
+		case result := <-resultChannel:
+			score += result
+		case <-timeoutChannel:
+			fmt.Println("Time's up!")
+			return score
+		}
+
+	}
+
+	return score
+}
+
 // main executes the quiz game.
 func main() {
 
@@ -50,20 +100,41 @@ func main() {
 		"Filepath (global) to quiz data.",
 	)
 
+	var timeLimit int
+	flag.IntVar(
+		&timeLimit,
+		"time_limit",
+		DEFAULT_TIME_LIMIT,
+		"Quiz duration (in seconds).",
+	)
+
 	flag.Parse()
 
-	quizData := getQuizData(filepath)
+	data := getQuizData(filepath)
 
-	var score int = 0
-	var user_response string
-	for i, row := range quizData {
-		fmt.Printf("%d. %s?\n", i+1, row[0])
-		fmt.Scan(&user_response)
+	fmt.Printf(
+		strings.Join(
+			[]string{
+				"You are about to take a quiz.\n",
+				"The timer is set to %d seconds.\n",
+				"Please press enter to begin.\n",
+			},
+			"",
+		),
+		timeLimit,
+	)
 
-		if user_response == row[1] {
-			score += 1
-		}
+	var userResponse string
+
+	// TODO: CHANGE TO BUFIO SO IT CAN ACCEPT STRINGS WITH SPACES.
+	fmt.Scanln(&userResponse)
+
+	if userResponse != "" {
+		fmt.Println("Text detected; terminating quiz.")
+		os.Exit(0)
 	}
 
-	fmt.Printf("\nYou scored %d out of %d.\n", score, len(quizData))
+	score := executeQuiz(data, timeLimit)
+
+	fmt.Printf("\nYou scored %d out of %d.\n", score, len(data))
 }

dd70b8949713820e4096f73e18b3492cb867b7c1
Chancellor Tran <chancetran98@gmail.com>
2023-01-04 20:15:12 -0800

    feat(urlshort): implement `MapHandler()`

--- 2-urlshort/handler.go
+++ 2-urlshort/handler.go
@@ -11,8 +11,16 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if path, ok := pathsToUrls[r.URL.String()]; ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return

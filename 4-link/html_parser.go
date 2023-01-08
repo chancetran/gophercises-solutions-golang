f38f4bbf413cbf7b016875bc0ed12262e1c1536b
Chancellor Tran <chancetran98@gmail.com>
2023-01-07 21:07:06 -0800

    feat(link): implement `ParseLinksFromHTTP`

---
+++ 4-link/html_parser.go
@@ -0,0 +1,93 @@
+package link
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+// ParseLinksFromHTTP parses the specified HTML file and
+// returns an array of Links.
+func ParseLinksFromHTTP(filepath string) []Link {
+
+	var links []Link
+
+	data, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dataHTML, err := html.Parse(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer data.Close()
+
+	var link_nodes []*html.Node
+
+	// getLinkNodes traverses through the HTML graph and appends
+	// 'link nodes' to `link_nodes`.
+	var getLinkNodes func(*html.Node)
+	getLinkNodes = func(n *html.Node) {
+
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					link_nodes = append(link_nodes, n)
+				}
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			getLinkNodes(c)
+		}
+	}
+	getLinkNodes(dataHTML)
+
+	for _, link_node := range link_nodes {
+		links = append(links, BuildLink(link_node))
+	}
+
+	return links
+}
+
+//
+func BuildLink(n *html.Node) Link {
+	var result Link
+
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			result.Href = attr.Val
+		}
+	}
+
+	// getLinkNodeText ....
+	var getLinkNodeText func(*html.Node) string
+	getLinkNodeText = func(n *html.Node) string {
+		if n.Type == html.TextNode {
+			return n.Data
+		}
+		if n.Type != html.ElementNode {
+			return ""
+		}
+
+		var result string
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			result += getLinkNodeText(c) + " "
+		}
+
+		return strings.Join(strings.Fields(result), " ")
+	}
+
+	result.Text = getLinkNodeText(n)
+
+	return result
+}

a70bed7500629129b38575a39c97b01d3e552827
Chancellor Tran <chancetran98@gmail.com>
2023-01-07 19:08:56 -0800

    chore(quiz): clean up `main.go`

--- 1-quiz/main/main.go
+++ 1-quiz/main/main.go
@@ -27,91 +27,6 @@ var DEFAULT_FILEPATH = "problems.csv"
 var DEFAULT_TIME_LIMIT = 30
 var DEFAULT_SHUFFLE = false
 
-// getUserInput uses Buffered I/O to read in user input from the command line.
-// This allows for user input with spaces.
-func getUserInput() string {
-
-	in := bufio.NewReader(os.Stdin)
-
-	result, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return result
-}
-
-// getQuizData reads in a file from the specified filepath and returns the
-// data.
-func getQuizData(filepath string) [][]string {
-
-	f, err := os.Open(filepath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return data
-}
-
-// executeQuiz loops over the specified data, displaying the first
-// row-attribute as the 'question', awaits user input, and then compares said
-// input with the second row-attribute as the 'answer'.
-//
-// If the user input matches the 'answer' the running total, or score, of
-// correct answers is incremented. Otherwise, the loop continues.
-//
-// The loop is terminated if the function execution time exceeds the
-// 'timeLimit'.
-func executeQuiz(data [][]string, timeLimit int) int {
-
-	resultChannel := make(chan int, 1)
-	timeoutChannel := time.After(time.Duration(timeLimit) * time.Second)
-
-	score := 0
-	for i, row := range data {
-
-		fmt.Printf("%d. %s?\n", i+1, row[0])
-
-		go func() {
-			userResponse := getUserInput()
-
-			processedUserResponse := strings.ReplaceAll(
-				strings.Trim(strings.ToLower(userResponse), "\n"), " ", "",
-			)
-
-			processedAnswer := strings.ReplaceAll(
-				strings.ToLower(row[1]), " ", "",
-			)
-
-			result := 0
-			if processedUserResponse == processedAnswer {
-				result = 1
-			}
-
-			resultChannel <- result
-		}()
-
-		select {
-		case result := <-resultChannel:
-			score += result
-		case <-timeoutChannel:
-			fmt.Println("Time's up!")
-			return score
-		}
-
-	}
-
-	return score
-}
-
 // main executes the quiz game.
 func main() {
 
@@ -123,14 +38,6 @@ func main() {
 		"Filepath (global) to quiz data.",
 	)
 
-	var timeLimit int
-	flag.IntVar(
-		&timeLimit,
-		"time_limit",
-		DEFAULT_TIME_LIMIT,
-		"Quiz duration (in seconds).",
-	)
-
 	var shuffleFlag bool
 	flag.BoolVar(
 		&shuffleFlag,
@@ -139,12 +46,21 @@ func main() {
 		"Shuffle the quiz data.",
 	)
 
+	var timeLimit int
+	flag.IntVar(
+		&timeLimit,
+		"time_limit",
+		DEFAULT_TIME_LIMIT,
+		"Quiz duration (in seconds).",
+	)
+
 	flag.Parse()
 
-	data := getQuizData(filepath)
+	data := getCSVData(filepath)
 
 	shuffleText := "Shuffling is off.\n"
 	if shuffleFlag {
+		data = shuffleCSVData(data)
 		shuffleText = "Shuffling is on.\n"
 	}
 
@@ -168,16 +84,119 @@ func main() {
 		os.Exit(0)
 	}
 
-	if shuffleFlag {
-		rand.Shuffle(
-			len(data),
-			func(i int, j int) {
-				data[i], data[j] = data[j], data[i]
-			},
-		)
-	}
-
 	score := executeQuiz(data, timeLimit)
 
 	fmt.Printf("\nYou scored %d out of %d.\n", score, len(data))
+}
+
+// getUserInput uses Buffered I/O to read in user input from the command line.
+// This allows for user input with spaces.
+func getUserInput() string {
+
+	in := bufio.NewReader(os.Stdin)
+
+	result, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+// getCSVData reads in a CSV file from the specified filepath
+// and returns the data.
+func getCSVData(filepath string) [][]string {
+
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+// shuffleCSVData returns a copy of the data but shuffled
+// psuedo-randomly using the default seed.
+func shuffleCSVData(CSVData [][]string) [][]string {
+
+	var data [][]string
+	copy(data, CSVData)
+
+	rand.Shuffle(
+		len(data),
+		func(i int, j int) {
+			data[i], data[j] = data[j], data[i]
+		},
+	)
+
+	return data
+}
+
+// executeQuiz loops over the specified data, displaying the first
+// row-attribute as the 'question', awaits user input, and then compares said
+// input with the second row-attribute as the 'answer'.
+//
+// If the user input matches the 'answer' the running total, or score, of
+// correct answers is incremented. Otherwise, the loop continues.
+//
+// The loop is terminated if the function execution time exceeds the
+// 'timeLimit'.
+func executeQuiz(data [][]string, timeLimit int) int {
+
+	resultChannel := make(chan int, 1)
+	timeoutChannel := time.After(time.Duration(timeLimit) * time.Second)
+
+	score := 0
+	for i, row := range data {
+
+		fmt.Printf("%d. %s?\n", i+1, row[0])
+
+		// A goroutine that prompts the user with a quiz question.
+		//
+		// The user response and the answer to the corresponding quiz question
+		// are stripped of formatting to ensure a valid comparison.
+		//
+		// This is done in a goroutine to allow for a timeout.
+		go func() {
+
+			userResponse := getUserInput()
+
+			processedUserResponse := strings.ReplaceAll(
+				strings.Trim(strings.ToLower(userResponse), "\n"), " ", "",
+			)
+
+			processedAnswer := strings.ReplaceAll(
+				strings.ToLower(row[1]), " ", "",
+			)
+
+			result := 0
+			if processedUserResponse == processedAnswer {
+				result = 1
+			}
+
+			resultChannel <- result
+		}()
+
+		select {
+
+		case result := <-resultChannel:
+			score += result
+
+		case <-timeoutChannel:
+			fmt.Println("Time's up!")
+			return score
+
+		}
+
+	}
+
+	return score
 }
